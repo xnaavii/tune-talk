e9187f0c7567e7eeed0a85639f8e591208a1be80
Ivan <petrovic.valpovo@gmail.com>
2024-06-15 11:54:20 +0100

    set up cloudinary

--- tunetalk/settings.py
+++ tunetalk/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7k8s$nvg#$ol0ycrwyh^$^br3qerah!a#jbga==n%&#_w#y_)w'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -41,6 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'cloudinary_storage',
+    'cloudinary',
     'homepage',
 ]
 
