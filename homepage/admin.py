90d41f5b9cec4eab31f5b13cf37241991e03e3e2
Ivan <petrovic.valpovo@gmail.com>
2024-06-15 15:27:49 +0100

    delete banner model

--- homepage/admin.py
+++ homepage/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
-from .models import Banner, TopAlbum
+from .models import TopAlbum
 
 # Register your models here.
-admin.site.register(Banner)
 admin.site.register(TopAlbum)
--- homepage/models.py
+++ homepage/models.py
@@ -1,15 +1,7 @@
 from django.db import models
 from cloudinary.models import CloudinaryField
 
-# Create your models here.
-class Banner(models.Model):
-    title = models.CharField(max_length=200)
-    image = CloudinaryField('image')
-    link = models.URLField(blank=True)
-
-    def __str__(self):
-        return self.title
-    
+# Create your models here. 
 class TopAlbum(models.Model):
     title = models.CharField(max_length=200)
     artist = models.CharField(max_length=100)
