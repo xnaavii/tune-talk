1ddd4b588d204b0cea9fd88c11475aab92fa7e26
Ivan <petrovic.valpovo@gmail.com>
2024-06-17 15:29:37 +0100

    create reviews app

--- tunetalk/settings.py
+++ tunetalk/settings.py
@@ -28,7 +28,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, "templates")
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
     'django_summernote',
     'homepage',
     'about',
+    'reviews',
 ]
 
 SITE_ID = 1
