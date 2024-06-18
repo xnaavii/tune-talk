1139d60dfec3126b23be51a7d80ef37fe1073f8a
Ivan <petrovic.valpovo@gmail.com>
2024-06-18 19:54:17 +0100

    place spotify api initialization into a function

--- reviews/views.py
+++ reviews/views.py
@@ -4,15 +4,23 @@ from django.http import HttpResponse
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 
-client_id = os.environ.get("SPOTIPY_CLIENT_ID")
-client_secret = os.environ.get("SPOTIPY_CLIENT_SECRET")
-auth_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
-sp = spotipy.Spotify(auth_manager=auth_manager)
-# Create your views here.
+
+def get_spotify_client():
+    """
+    Set up spotify client credentials manager
+    """
+    client_id = os.environ.get("SPOTIPY_CLIENT_ID")
+    client_secret = os.environ.get("SPOTIPY_CLIENT_SECRET")
+    auth_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
+
+    return spotipy.Spotify(auth_manager=auth_manager)
+
 def search_album(request):
     """
     Query album data from spotify based on the search_query
     """
+    sp = get_spotify_client()
+
     if request.method == "GET":
 
         search_term = request.GET.get("search_term")
