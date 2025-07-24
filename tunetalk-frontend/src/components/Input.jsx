e738e51babb10bd5d6e2e8e58420883ed171a761
Ivan <petrovic.valpovo@gmail.com>
2025-07-24 19:28:53 +0100

    Add navigation to logo on click and to input field on submit

--- tunetalk-frontend/src/components/Input.jsx
+++ tunetalk-frontend/src/components/Input.jsx
@@ -1,15 +1,25 @@
+import { useNavigate } from 'react-router-dom';
+
 export default function Input({ type = 'text', ...props }) {
+  const navigate = useNavigate();
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    navigate('/results');
+  }
+
   return (
-    <div className='relative w-110'>
+    <form className='relative w-110' onSubmit={handleSubmit}>
       <input
         type={type}
         {...props}
         className='w-full py-3 pr-10 pl-4 text-stone-100 border-stone-50 rounded-3xl shadow-sm backdrop-blur-md bg-[#C2E1FA]/20 outline-none focus:ring-2 focus:ring-stone-100 focus:ring-inset'
       />
+
       <ion-icon
         name='search-outline'
         class='absolute right-3 top-1/2 -translate-y-1/2 text-xl text-stone-100 pointer-events-none'
       />
-    </div>
+    </form>
   );
 }
--- tunetalk-frontend/src/components/Logo.jsx
+++ tunetalk-frontend/src/components/Logo.jsx
@@ -1,7 +1,17 @@
+import { useNavigate } from 'react-router-dom';
+
 export default function Logo() {
+  const navigate = useNavigate();
+
+  function handleOnClick() {
+    navigate('/');
+  }
+
   return (
     <h1 className={'tracking-tighter text-7xl font-extrabold text-stone-50'}>
-      Tunetalk
+      <button onClick={handleOnClick} className=' hover:cursor-pointer'>
+        Tunetalk
+      </button>
     </h1>
   );
 }
