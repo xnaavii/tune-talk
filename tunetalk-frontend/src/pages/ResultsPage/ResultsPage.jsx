d871a58c8428308e61c7c2e0108c9d8616e33560
Ivan <petrovic.valpovo@gmail.com>
2025-08-02 10:11:56 +0100

    Create album page

--- tunetalk-frontend/src/pages/ResultsPage/ResultsPage.jsx
+++ tunetalk-frontend/src/pages/ResultsPage/ResultsPage.jsx
@@ -1,4 +1,3 @@
-import Navbar from '../../components/Navbar';
 import ResultsContainer from '../../components/ResultsContainer';
 import { useSearchParams } from 'react-router-dom';
 import { useState, useEffect } from 'react';
--- tunetalk-frontend/src/routes/AppRoutes.jsx
+++ tunetalk-frontend/src/routes/AppRoutes.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 import LandingPage from '../pages/LandingPage/LandingPage';
 import MainLayout from '../layouts/MainLayout';
 import ResultsPage from '../pages/ResultsPage/ResultsPage';
+import AlbumPage from '../pages/AlbumPage/AlbumPage';
 
 export default function AppRoutes() {
   return (
@@ -10,6 +11,7 @@ export default function AppRoutes() {
 
       <Route element={<MainLayout />}>
         <Route path='/results' element={<ResultsPage />} />
+        <Route path='/results/album' element={<AlbumPage />} />
       </Route>
     </Routes>
   );
