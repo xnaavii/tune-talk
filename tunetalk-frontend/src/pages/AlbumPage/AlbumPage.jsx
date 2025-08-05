cef798ca0054bf8e41e275266afca7b72fd74100
Ivan <petrovic.valpovo@gmail.com>
2025-08-05 10:28:12 +0100

    Fix responsivity on the album page

--- tunetalk-frontend/src/pages/AlbumPage/AlbumPage.jsx
+++ tunetalk-frontend/src/pages/AlbumPage/AlbumPage.jsx
@@ -12,15 +12,15 @@ export default function AlbumPage() {
   return (
     <div className='p-3'>
       <Button icon='chevron-back' />
-      <div className='flex items-start gap-6 mb-6 mt-2'>
-        <figure className='flex flex-row gap-6 p-4'>
+      <div className='flex gap-6 mb-6 mt-2'>
+        <figure className='flex flex-col md:flex-row gap-6 p-4 '>
           <img
             src={album.image}
             alt={`Cover of ${album.title}`}
-            className='w-70 aspect-square object-cover rounded-md shadow-sm'
+            className='object-cover rounded-md shadow-sm w-full flex-1/3'
           />
 
-          <figcaption className='flex flex-col justify-center text-stone-200'>
+          <figcaption className='flex flex-col justify-center text-stone-200 flex-1/2'>
             <h2 className='text-2xl font-bold mb-1'>{album.title}</h2>
             <h3 className='text-lg font-medium text-stone-300 mb-2'>
               {album.artist}
