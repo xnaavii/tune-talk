3acff9f364cf750811c4e208d9370895bfa89b07
Ivan <petrovic.valpovo@gmail.com>
2025-08-15 10:22:28 +0100

    Add review section changes to the album page and revision the whole layout improvements

--- tunetalk-frontend/src/pages/AlbumPage/AlbumPage.jsx
+++ tunetalk-frontend/src/pages/AlbumPage/AlbumPage.jsx
@@ -27,16 +27,18 @@ export default function AlbumPage() {
     return <p className='text-center text-stone-300 mt-8'>Album not found</p>;
 
   return (
-    <div className='p-3 w-full grid grid-rows-[auto_1fr_1fr]'>
-      <div>
+    <div className='w-full h-full p-2 flex flex-col gap-2'>
+      {/* Back Button */}
+      <div className='flex items-center justify-start shrink-0'>
         <Button icon='chevron-back' onClick={() => navigateBack()} />
       </div>
+
       {/* Album Details */}
-      <figure className='grid grid-cols-1 md:grid-cols-[1fr_2fr] gap-6 p-4 w-full'>
+      <figure className='grid grid-cols-1 md:grid-cols-[1fr_2fr] gap-6 p-4 w-full shrink-0'>
         <img
           src={album.image}
           alt={`Cover of ${album.title}`}
-          className='object-cover rounded-md shadow -lg w-full'
+          className='object-cover rounded-md shadow-lg w-full'
         />
 
         <figcaption className='flex flex-col justify-center text-stone-200'>
@@ -53,8 +55,25 @@ export default function AlbumPage() {
       </figure>
 
       {/* Review Section */}
-      <section>
-      <p>Review User</p>
+      <section className='flex-1 flex flex-col min-h-0'>
+        <h2 className='text-2xl mb-1 shrink-0'>Reviews</h2>
+
+        <div className='flex-1 overflow-y-auto backdrop-blur-sm bg-[#C2E1FA]/20 p-2 rounded-xl flex flex-col gap-2'>
+          <div className='flex items-center justify-between bg-[#C2E1FA]/10 rounded-lg gap-1 h-20 p-2 shrink-0'>
+            <textarea
+              name='review'
+              id='review'
+              placeholder='Write a review'
+              maxLength={150}
+              className='w-full h-full resize-none text-stone-50 p-2 outline-none focus:ring-1 focus:ring-stone-200 focus:ring-inset focus:bg-[#C2E1FA]/30 border border-stone-300 rounded-md text-sm bg-[#C2E1FA]/20'
+            ></textarea>
+            <Button icon='add' />
+          </div>
+
+          <p className='text-sm text-stone-200 self-center'>
+            No reviews yet for this album
+          </p>
+        </div>
       </section>
     </div>
   );
