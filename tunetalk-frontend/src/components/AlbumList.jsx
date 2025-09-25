ddba452bd2b4abf006193b8d26b1563a72e88398
Ivan <petrovic.valpovo@gmail.com>
2025-09-25 23:39:06 +0100

    fix albumlist grid

--- tunetalk-frontend/src/components/AlbumList.jsx
+++ tunetalk-frontend/src/components/AlbumList.jsx
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 export default function AlbumList({ albums }) {
   return (
     <section className='flex-1 overflow-auto'>
-      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-3'>
+      <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 gap-[10px]'>
         {albums.map((album) => (
           <AlbumCard key={album.id} albumId={album.id} />
         ))}
